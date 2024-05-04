8837c2209f4ffd5ba9abc19ba144f7c207a0e389
ganeshshinde0612 <165639734+ganeshshinde0612@users.noreply.github.com>
2024-05-04 20:23:49 +0530

    by ganesh

--- gitproject/src/com/gitproject/gitDemo.java
+++ gitproject/src/com/gitproject/gitDemo.java
@@ -2,6 +2,7 @@ package com.gitproject;
 
 public class gitDemo {
 	 int a =20;
+	int b = 30;
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("java");
